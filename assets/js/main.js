3c2256256734ac07b88f780c208a143a0c25f0aa
salah <cloud.data.sa@gmail.com>
2023-07-12 03:05:57 +0300

    last 27

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,6 +78,10 @@
 			autoplay: {
 				delay: 5000,
 			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
 			navigation: {
 				nextEl: ".swiper-button-next",
 				prevEl: ".swiper-button-prev",
@@ -105,6 +109,11 @@
 			loop: true,
 			autoplay: {
 				delay: 5000,
+			},
+			
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
 			},
 			navigation: {
 				nextEl: ".swiper-button-next",
